070387c314e128ed5f921491a0d1f3cb0c886632
Danilchenko Mihail <mihaildanilchenko85@gmail.com>
2025-03-20 21:25:38 +0300

    add 2 chaeld classes

--- exersizes.py
+++ exersizes.py
@@ -21,6 +21,17 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        # self.name = name
+        # self.surname = surname
+        # self.courses_attached = []
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
  
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
