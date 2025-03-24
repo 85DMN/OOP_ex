0cd90ded696e9bed0e6af579e25ec5d6321900b9
Danilchenko Mihail <mihaildanilchenko85@gmail.com>
2025-03-24 18:40:43 +0300

    add code for use all classes and methods for it + function for calculalate medium values in all exersizes and lections

--- exersizes.py
+++ exersizes.py
@@ -62,14 +62,101 @@ class Reviewer(Mentor):
     def __str__(self):
         return f'Имя: {self.name}\nФамилия: {self.surname}'
  
-# best_student = Student('Ruoy', 'Eman', 'your_gender')
-# best_student.courses_in_progress += ['Python']
+students, lectorers = [],[]
+
+best_student = Student('Ryou','Eman','man')
+best_student.courses_in_progress += ['Python']
+best_student.courses_in_progress += ['Python_ML']
+students.append(best_student) 
+
+best_student1 = Student('Natan','Ivanov','man')
+best_student1.courses_in_progress += ['Python']
+best_student1.courses_in_progress += ['Python_ML'] 
+students.append(best_student1)
+
+best_student2 = Student('Emma','Petrova','woman')
+best_student2.courses_in_progress += ['Python']
+best_student2.courses_in_progress += ['Python_ML'] 
+students.append(best_student2)
+
+reviwer = Reviewer("Some",'Buddy')
+reviwer.courses_attached += ['Python']
+reviwer.courses_attached += ['Python_ML']
+
+reviwer1 = Reviewer("Ann",'Hunt')
+reviwer1.courses_attached += ['Python']
+reviwer1.courses_attached += ['Python_ML']
+
+reviwer2 = Reviewer("Nik",'Spot')
+reviwer2.courses_attached += ['Python']
+reviwer2.courses_attached += ['Python_ML']
+
+lector = Lecturer("Any",'Where')
+lector.courses_attached += ['Python']
+lector.courses_attached += ['Python_ML']
+lectorers.append(lector)
+
+lector1 = Lecturer("Mikhael",'Hunt')
+lector1.courses_attached += ['Python']
+lector1.courses_attached += ['Python_ML']
+lectorers.append(lector1)
+
+lector2 = Lecturer("Sam",'Spot')
+lector2.courses_attached += ['Python']
+lector2.courses_attached += ['Python_ML']
+lectorers.append(lector2)
+
+reviwer.rate_hw(best_student,'Python',10)
+reviwer.rate_hw(best_student1,'Python',10)
+reviwer.rate_hw(best_student2,'Python',10)
+reviwer1.rate_hw(best_student,'Python',10)
+reviwer1.rate_hw(best_student1,'Python',10)
+reviwer1.rate_hw(best_student2,'Python',10)
+reviwer2.rate_hw(best_student,'Python',9)
+reviwer2.rate_hw(best_student1,'Python',9)
+reviwer2.rate_hw(best_student2,'Python',9)
+
+reviwer.rate_hw(best_student,'Python_ML',9)
+reviwer.rate_hw(best_student1,'Python_ML',9)
+reviwer.rate_hw(best_student2,'Python_ML',9)
+reviwer1.rate_hw(best_student,'Python_ML',9)
+reviwer1.rate_hw(best_student1,'Python_ML',9)
+reviwer1.rate_hw(best_student2,'Python_ML',9)
+reviwer2.rate_hw(best_student,'Python_ML',10)
+reviwer2.rate_hw(best_student1,'Python_ML',10)
+reviwer2.rate_hw(best_student2,'Python_ML',10)
  
-# cool_mentor = Mentor('Some', 'Buddy')
-# cool_mentor.courses_attached += ['Python']
+best_student.ocenka_r(lector,'Python',10)
+best_student.ocenka_r(lector1,'Python',10)
+best_student.ocenka_r(lector2,'Python',10)
+best_student1.ocenka_r(lector,'Python',10)
+best_student1.ocenka_r(lector1,'Python',10)
+best_student1.ocenka_r(lector2,'Python',10)
+best_student2.ocenka_r(lector,'Python',9)
+best_student2.ocenka_r(lector1,'Python',9)
+best_student2.ocenka_r(lector2,'Python',9)
+
+best_student.ocenka_r(lector,'Python_ML',10)
+best_student.ocenka_r(lector1,'Python_ML',10)
+best_student.ocenka_r(lector2,'Python_ML',10)
+best_student1.ocenka_r(lector,'Python_ML',10)
+best_student1.ocenka_r(lector1,'Python_ML',10)
+best_student1.ocenka_r(lector2,'Python_ML',10)
+best_student2.ocenka_r(lector,'Python_ML',9)
+best_student2.ocenka_r(lector1,'Python_ML',9)
+best_student2.ocenka_r(lector2,'Python_ML',9)
+
+def srednee_po_vsem(parametr):
+    statistika = {}
+    for j in parametr:
+        ty = list(j.grades.keys())
+        for g in ty:
+            if statistika.get(g) == None:
+                statistika[g]=(sum(j.grades[g])/len(j.grades[g]))
+            else:
+                statistika[g]=(sum(j.grades[g])/len(j.grades[g])+statistika[g])/2                
+    return statistika
+
+print(srednee_po_vsem(students))
  
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-# cool_mentor.rate_hw(best_student, 'Python', 10)
- 
-print(best_student.grades)
+# print(best_student.grades)
