0439a7008a5e6953a83203553f61703d10fc2b0e
Danilchenko Mihail <mihaildanilchenko85@gmail.com>
2025-03-24 18:36:19 +0300

    add __str__ for 3 classes

--- exersizes.py
+++ exersizes.py
@@ -6,6 +6,24 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+
+    def __str__(self):
+        m,l=0,0
+        for gh in self.grades.values():
+            m += sum(gh)
+            l += len(gh)
+        delim, delim_f = ', '.join(self.courses_in_progress),', '.join(self.finished_courses)
+        m = f'Имя: {(self.name)}\nФамилия: {(self.surname)}\nСредняя оценка за домашние задания: {m/l}\nКурсы в процессе изучения: {delim}\nЗавершенные курсы: {delim_f}'
+        return m
+
+    def ocenka_r(self, lecturer, course, grade):
+        if isinstance(lecturer,Mentor) and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return "Ошибка"
         
 class Mentor:
     def __init__(self, name, surname):
@@ -29,10 +47,20 @@ class Lecturer(Mentor):
         # self.name = name
         # self.surname = surname
         # self.courses_attached = []
+    
+    def __str__(self):
+        m,l=0,0
+        for gh in self.grades.values():
+            m += sum(gh)
+            l += len(gh)
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {m/l}'
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
+    
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}'
  
 # best_student = Student('Ruoy', 'Eman', 'your_gender')
 # best_student.courses_in_progress += ['Python']
